71a105cc7ec28a2adb565cf29c7d1bf0b983b1e4
RiccardoForni <67865525+RiccardoForni@users.noreply.github.com>
2023-10-27 14:37:41 +0200

    Update Regression_Code.py

--- Project - Mazzolin, Forni, Dian, Lavarello/Regression_Code.py
+++ Project - Mazzolin, Forni, Dian, Lavarello/Regression_Code.py
@@ -10,6 +10,7 @@ Retrieve Subset equity and Interest rate
 """
 Subset= pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="Subset")
 Interest = pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="EURIBOR_3_M")
+Interest_BUND = pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="BUND")
 
 
 """
@@ -17,6 +18,8 @@ Calculate interest rate
 """
 RFREE=Interest[["BD INTEREST RATES - EURIBOR RATE - 3 MONTH NADJ"]]/12
 RF = np.array(RFREE)
+BUNDRISK=Interest_BUND[['RF GERMANY GVT BMK BID YLD 3M - RED. YIELD']]/12
+BRF = np.array(BUNDRISK)
 
 """
 Clear from Date
@@ -41,34 +44,49 @@ Calculate for each equity: Equity-interest rate
 The result is a matrix [Number of equity][Value calculated]
 """
 result = []
+result_with_bund = []
 for E in Equity:
     result.append(np.subtract(E,RFREE)) #Subtract operation filled inside an array 
+    result_with_bund.append(np.subtract(E,BRF))
 
 n=np . size ( Equity[0]) #Constant dimension
 i=0
 for e in np.array(Subset_Clear).T:
     str=Subset_Clear.columns[i].replace(" - TOT RETURN IND","")
     plt.figure()
-    plt.plot(t , e[1:n],t,RF[1:n])
+    plt.plot(t , e[1:n])
     plt . xlabel ('Time - Monthly - 30-09-2013 - 30-09-2023 ')
-    plt . ylabel (str+' Monthly Total Ret and Risk Free')
+    plt . ylabel (str+' Monthly Total Ret')
     plt.savefig("img/Total_Return/TR-"+str+".png")
-    if i == 20 :
-        plt.close()
+    if i == 15 :
+        plt.close('all')
     i+=1
-plt.close()
+plt.close('all')
 i=0
 for e in result:
     str=Subset_Clear.columns[i].replace(" - TOT RETURN IND","")
     plt.figure()
-    plt.plot(t , e[1:n],)
+    plt.plot(t , e[1:n],t,RF[1:n])
     plt . xlabel ('Time - Monthly - 30-09-2013 - 30-09-2023 ')
-    plt . ylabel (str+' Excess Returns')
+    plt . ylabel (str+' Excess Returns and risk free')
     plt.savefig("img/Excess_Return/ER-"+str+".png")
-    if i == 20 :
-        plt.close()
+    if i == 15 :
+        plt.close('all')
     i+=1
-plt.close()
+plt.close('all')
+
+i=0
+for e in result_with_bund:
+    str=Subset_Clear.columns[i].replace(" - TOT RETURN IND","")
+    plt.figure()
+    plt.plot(t , e[1:n],t,BRF[1:n])
+    plt . xlabel ('Time - Monthly - 30-09-2013 - 30-09-2023 ')
+    plt . ylabel (str+' Excess Returns and risk free(BUND)')
+    plt.savefig("img/Excess_Return/BUND/ERB-"+str+".png")
+    if i == 15 :
+        plt.close('all')
+    i+=1
+plt.close('all')
     
 
 
