9ed2b35477d3f6b4819e186e62bbcb82cbce75aa
RiccardoForni <67865525+RiccardoForni@users.noreply.github.com>
2023-11-03 13:27:06 +0100

    Update Regre_Function.py

--- Project - Mazzolin, Forni, Dian, Lavarello/Regre_Function.py
+++ Project - Mazzolin, Forni, Dian, Lavarello/Regre_Function.py
@@ -1,81 +1,63 @@
-from matplotlib.ticker import PercentFormatter
-import matplotlib.pyplot as plt
-import statsmodels . api as sm
-import pandas as pd
-import os
 import numpy as np
-def plotbar(P,string):
-    for e in P:
-        plt.bar(e[0],e[1])
-        plt.xticks(rotation=90)
-    plt.savefig("TEST"+string+".png")
-
-def plotCAPM(Stocks,Market,OLSResult,Subset,String):
-    cwd = os.getcwd()
-    folder = cwd + "/img/testCAPM/"
-
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-    myint=iter(Subset.columns)
-    for e,OLSRes in zip(Stocks,OLSResult):
-        str=next(myint).strip()
-        plt.figure()
-        plt.plot(Market, OLSRes.iloc[1][0]*Market+OLSRes.iloc[0][1])
-        plt.scatter(Market,e)
-        plt . xlabel ('Eurostoxx')
-        plt . ylabel (str)
-        plt.savefig("img/testCAPM/CAPM-"+str+String+".png")
-        plt.close()
+import pandas as pd
+import Regre_Function as rf
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def plotscatter(setx,sety,title,xlabel,ylabel,sigla,Subset,string_to_save):
-    cwd = os.getcwd()
-    folder = cwd + "/"+string_to_save
+t=pd . date_range ( start ='15-09-2013 ',end ='15-09-2023 ', freq ='M') #Date series
 
-    if not os.path.exists(folder):
-        os.mkdir(folder)
+"""
+Retrieve Subset equity and Interest rate
+"""
+sheet=-1
+col=""
+while sheet < 0 or sheet>1:
+    sheet=int(input("che tasso di Risk-Free vuoi? premi 0 per Euribor o 1 per Bund"))
+
+match sheet:
+    case 0:
+         sheet = "EURIBOR_3_M"
+         col="BD INTEREST RATES - EURIBOR RATE - 3 MONTH NADJ"
+    case 1:
+        sheet = "BUND"
+        col="RF GERMANY GVT BMK BID YLD 3M - RED. YIELD"
         
-    myint=iter(Subset.columns)
-    for e in sety:
-        str=next(myint)
-        plt.figure()
-        plt.scatter(setx,e)
-        plt.title(title)
-        plt . xlabel (xlabel)
-        plt . ylabel (str+ylabel)
-        plt.savefig(folder+"/"+sigla+"-"+str+".png")
-        plt.close()
-      
+
+EuroStoxx = pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="EUROSTOXX600")
+Subset_Stock_Selected= pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="Subset")
+Interest = pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name=sheet)
 
 
-def OLS(Stock_Risk_Free,Market,printSummary=False):
-    Res= []
-    X = np . column_stack (( np . ones_like ( Market ) , Market ))
-    try:
-        Stock_Risk_Free.shape[1]
-        for e,i in zip(Stock_Risk_Free,range(0,len(Stock_Risk_Free))):
-            df = sm . OLS ( e[1:] , X[1:]  ). fit ()
-            Res.append(pd.read_html(df.summary().tables[1].as_html(),header=0,index_col=0)[0])
-            if printSummary:
-               with open('summary'+str(i)+'.txt', 'w') as fh:
-                    fh.write(df.summary().as_html())
-    except:
-        Res.append(pd.read_html(sm . OLS (  Stock_Risk_Free[1:], X[1:]  ). fit ().summary().tables[1].as_html(),header=0,index_col=0)[0])
-        
-    
-    return Res
+Subset_Stock_Selected.columns = Subset_Stock_Selected.columns.str.replace("- TOT RETURN IND","")
+EuroStoxx = EuroStoxx.loc[:, EuroStoxx.columns != 'Name']#Delete column of date
+Subset_Stock_Selected=Subset_Stock_Selected.loc[:, Subset_Stock_Selected.columns != 'Name'] #Delete column of date
 
-def ReorderByOLSParam(Stocks,Subset,Row_interess,Coloum_interess):
-    """
-    Function return stock reorder by OLS result
-    Row_interess -> choose row of OLS Summary between
-    Const=0, X1=1
+RFREE=np.array(Interest[[col]]/12)
+"""
+Clear from Date
+and
+calculate Log operation 
+"""
+Market =  np.array(100 *( np . log ( EuroStoxx ) -np . log ( EuroStoxx . shift (1) )) )
+Equities = np.array(100 *( np . log ( Subset_Stock_Selected ) -np . log ( Subset_Stock_Selected . shift (1) ))) #Log operation
+"""
+Calculate for each equity: Equity-interest rate
 
-    Coloum_interess-> choose coloum of OLS summary between
-    coef=0      std err=1    t=2     P>|t|=3     0.025=4   0.975=5  
-    """ 
-    P = {}
-    myint=iter(Subset.columns)
-    for e in Stocks:
-            P[next(myint)]=e.iloc[Row_interess][Coloum_interess]
-    return sorted(P.items(), key=lambda x:x[1])
+The result is a matrix [Number of equity][Value calculated]
+"""
+rStock = np.subtract(Equities,RFREE).T #Subtract operation filled inside an array 
+rMarket =np.subtract(Market,RFREE)
+
+rf.plotscatter(Market,rStock,"Excess Returns vs Eurostoxx -"+sheet,
+            "Time - Monthly - 30-09-2013 - 30-09-2023",
+            "",sheet,Subset_Stock_Selected,
+            "Excess_return"
+            )
+
+Res_Euribor= rf.OLS(rStock,rMarket,False)
+P_sort=rf.ReorderByOLSParam(Res_Euribor,Subset_Stock_Selected,0,3)
+rf.plotbar(P_sort,sheet)
+rf.plotCAPM(rStock,rMarket,Res_Euribor,Subset_Stock_Selected,sheet)
+Excess_equi_valued=sum(rStock)/len(rStock)
+Excess_OLS=rf.OLS(Excess_equi_valued,rMarket)
