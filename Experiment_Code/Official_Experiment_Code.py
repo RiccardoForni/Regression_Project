ecf5976d5d6b8d430a6dea13544ccc3324bbb4aa
francescomazzolin <149055581+francescomazzolin@users.noreply.github.com>
2023-11-05 15:48:32 +0100

    Add files via upload

---
+++ Experiment_Code/Official_Experiment_Code.py
@@ -0,0 +1,183 @@
+import numpy as np
+import pandas as pd
+import Official_Experiment_Function as rf
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+"""
+Creating time-series of relevant dates
+"""
+t=pd . date_range ( start ='15-09-2013 ',end ='15-09-2023 ', freq ='M') #Date series
+
+"""
+Retrieve Subset equity and Interest rate
+"""
+sheet=-1
+col=""
+while sheet < 0 or sheet>1:
+    sheet=int(input("Which Risk Free rate you want? press 0 for Euribor or 1 for Bund: "))
+
+match sheet:
+    case 0:
+         sheet = "EURIBOR_3_M"
+         col="BD INTEREST RATES - EURIBOR RATE - 3 MONTH NADJ"
+    case 1:
+        sheet = "BUND"
+        col="RF GERMANY GVT BMK BID YLD 3M - RED. YIELD"      
+
+
+EuroStoxx = pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="EUROSTOXX600")
+Subset_Stock_Selected= pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name="Subset")
+Interest = pd . read_excel('DataEuroStock_Tecnology.xlsx',sheet_name=sheet)
+
+"""
+Clean the data 
+Obtain monthly rates from annualized interest rates time-series
+"""
+
+Subset_Stock_Selected.columns = Subset_Stock_Selected.columns.str.replace(" - TOT RETURN IND","")
+EuroStoxx = EuroStoxx.loc[:, EuroStoxx.columns != 'Name']#Delete column of date
+Subset_Stock_Selected=Subset_Stock_Selected.loc[:, Subset_Stock_Selected.columns != 'Name'] #Delete column of date
+0
+RFREE=np.array(Interest[[col]]/12)
+
+
+"""
+a) Computing the logarithmic excess returns for:
+    1) EuroStoxx600 Total Return Index
+    2) Sample of stocks
+
+b) Inserting them in a dataframe
+"""
+stock_names = list(Subset_Stock_Selected.columns)
+print(type(np.subtract(100*np.log(EuroStoxx) - np . log ( EuroStoxx . shift (1) ),RFREE)) )
+
+df_factors = pd.DataFrame(data = 
+                         np.subtract(np.array(100 *( np . log ( EuroStoxx ) 
+                                     -np . log ( EuroStoxx . shift (1) )) ),
+                                   RFREE),
+                      columns = ['Market'])
+df_factors = df_factors.iloc[1:,]
+df_factors.index = t
+
+del EuroStoxx
+
+
+df_stocks = pd.DataFrame(data = 
+                         np.subtract(np.array(100 *( np . log ( Subset_Stock_Selected ) 
+                                     -np . log ( Subset_Stock_Selected . shift (1) )) ),
+                                   RFREE),
+                      columns = stock_names)
+df_stocks = df_stocks.iloc[1:,]
+df_stocks.index = t
+
+del Subset_Stock_Selected, RFREE,Interest,col
+
+"""
+Here in the argument title the content of the string: "sheet" is added
+"""
+
+l = rf.plotscatter(df_factors,df_stocks,"Excess Returns vs Eurostoxx -"+sheet,
+            "Market Excess Returns",
+            "",sheet,
+            "2_Excess_return"
+            )
+
+"""
+Here we estimate the CAPM model for each stock.
+    -CAPM_summary is a dataframe in which we stored the alphas, betas plus their
+    respective p-values and the R-Squared of each estimation.
+    -CAPM_list is a list containing the complete output of each linear regression
+"""
+
+CAPM_summary, CAPM_list = rf.OLS(df_stocks,df_factors)
+
+CAPM_summary.loc['Mean'] = CAPM_summary.mean()
+CAPM_summary = CAPM_summary.sort_values('p-value_alpha')
+
+CAPM_summary.to_excel("3_CAPM_stocks.xlsx")
+
+
+"""
+Here we create a (2,3) plot with:
+    row 1: the 3 scatter plots showing the best linear approximation
+    row 2: the 3 scatter plots showing the worst linear approximation
+"""
+CAPM_summary = CAPM_summary.sort_values('R-Squared')
+
+rf.m_scatter(CAPM_summary, df_factors, df_stocks,
+             sheet,
+             "2_scatter_comparison")
+
+"""
+P-values analysis
+"""
+
+rf.plotbar(CAPM_summary['p-value_alpha'])
+
+rf.plotbar(CAPM_summary['p-value_beta: Market'])
+
+
+
+"""
+THIS USES THE PREVIOUSLY DEFINED CODE TO PLOT THE OLS FITTED LINE IN THE 
+SCATTER PLOTS
+"""
+
+rf.plotCAPM(df_stocks,
+            df_factors,
+            stock_names,
+            CAPM_summary.loc[CAPM_summary.index != 'Mean'],
+            sheet)
+
+"""
+Computes excess returns of a equally-weighted portfolio for each month
+"""
+array_stocks = np.array(df_stocks.T)
+Excess_equi_valued = np.array(sum(array_stocks)/len(array_stocks)).reshape(-1,1)
+
+df_portfolios = pd.DataFrame(data = Excess_equi_valued, 
+                            columns = ['Portfolio - EW'],
+                            index = t)
+del Excess_equi_valued
+
+"""
+Runs a CAPM using as independent variable the excess return of said portfolio
+"""
+
+CAPM_EW_Portfolio_Summary, CAPM_EW_Portfolio = rf.OLS(
+                             df_portfolios['Portfolio - EW'],
+                             df_factors['Market'])
+
+"""
+Comparison between:
+    1) the average value of the parameters obtained for the single stocks
+    2) the value of the parameters obtained using the equally-weighted portfolio
+"""
+
+comparison_stocks_EW_portfolio = pd.concat([CAPM_summary.loc['Mean',:], 
+                        CAPM_EW_Portfolio_Summary.iloc[0,:]],
+                       axis = 1).T
+
+
+rf.comparison_scatter_2(df_stocks,df_portfolios['Portfolio - EW'],
+            df_factors['Market'],
+            CAPM_EW_Portfolio_Summary,
+            "Excess Returns vs Eurostoxx -"+sheet,
+            "Market Excess Returns",
+            "Excess Returns",sheet,
+            "3_Comparison_Scatter"
+            )
+
+rf.comparison_scatter(df_stocks,df_portfolios['Portfolio - EW'],
+            df_factors['Market'],
+            "Comparison between the scatter plots",
+            "Market Excess Returns(" +sheet+")",
+            "Excess Returns(" +sheet+")",sheet,
+            "3_Comparison_Scatter"
+            )
+
+
+comparison_stocks_EW_portfolio =comparison_stocks_EW_portfolio.T
+
+comparison_stocks_EW_portfolio.to_excel("3_Comparison.xlsx")
---
+++ Experiment_Code/Official_Experiment_Function.py
@@ -0,0 +1,260 @@
+from matplotlib.ticker import PercentFormatter
+import matplotlib.pyplot as plt
+import statsmodels . api as sm
+import pandas as pd
+import os
+import numpy as np
+
+def plotbar(P):
+    
+    cwd = os.getcwd()
+    folder = cwd + "/3_p_value_plots/"
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    
+    variable = P.name
+    P = pd.DataFrame(data = P, columns = [variable])
+    mean = P.loc['Mean', variable]
+    P['stock_names'] = P.index
+
+    def bar_highlight(value, one_value, 
+                      five_value,
+                      ten_value,
+                      mean):
+        if value <= one_value:
+            return 'red'
+        elif value <= five_value:
+            return 'orange'
+        elif value <= ten_value:
+            return 'yellow'
+        elif value == mean:
+            return 'black'
+        else:
+            return 'grey'
+    fig, ax = plt.subplots()
+    
+    one_value = 0.01
+    five_value = 0.05
+    ten_value = 0.1
+    
+    P['colors'] = P[variable].apply(bar_highlight, args = (one_value, 
+                          five_value,
+                          ten_value,
+                          mean))
+
+    bars = plt.bar(P['stock_names'], P[variable], color=P['colors'])
+    x_pos = range(P['stock_names'].shape[0])
+    plt.xticks(x_pos, P['stock_names'], rotation=90)
+    
+
+    plt.savefig(folder+"/"+variable+".png")
+    plt.show()
+    
+    plt.close()
+
+
+def plotCAPM(rStock,Market,stock_names,OLSRes,String):
+    cwd = os.getcwd()
+    folder = cwd + "/2_testCAPM/"
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    for e in stock_names:
+
+        plt.figure()
+        plt.plot(Market, OLSRes.loc[e, 'beta: Market']*Market+OLSRes.loc[e, 'Alpha'])
+        plt.scatter(Market,rStock[e])
+        plt . xlabel ('Eurostoxx')
+        plt . ylabel (e)
+        plt.savefig("2_testCAPM/CAPM-"+e+String+".png")
+        plt.close()
+
+"""
+- setx = market return
+- set y = stock returns
+- title = title of the graph
+- xlabel = name of the x-axis
+- ylabel = name of the y-axis
+- sigla = unique name (either ER for euribor or ERB for bunds) to differentiate
+          between plots of excess returns of stocks computed with Euribor
+          or with the bund yield.
+- Subset = dataframe with the data of total index returns
+- string_to_save = name of the folder in which the graph will be saved in the 
+                   working directory
+"""
+
+def plotscatter(setx,sety,title,xlabel,ylabel,sigla,string_to_save):
+    cwd = os.getcwd()
+    folder = cwd + "/"+string_to_save
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        
+    l = pd.DataFrame(index = sety.columns, columns= ['Plot'])
+        
+    for e in sety.columns:
+
+        fig = plt.figure()
+        
+        ax1 = fig.add_subplot()
+        
+        ax1.scatter(setx,sety[e])
+        plt.title(title)
+        plt . xlabel (xlabel)
+        plt . ylabel (e+ylabel)
+        plt.savefig(folder+"/"+sigla+"-"+e+".png")
+        l.loc[e,'Plot'] = fig
+        plt.close()
+    return l
+
+def OLS(y, *x):
+
+    try:
+        
+        intercept = pd.Series(data = np.ones_like(x[0]), name = "intercept",
+                              index = x[0].index)
+        
+    except:
+        
+        intercept = pd.DataFrame(data = np.ones(y.shape[0] ), 
+                              columns = ["intercept"],
+                              index = y.index)
+    
+    try:
+        X = pd.DataFrame([intercept, *x]).T
+
+    except:
+        X = pd.concat([intercept,*x],axis = 1)
+
+
+    exog_names = list(X.columns)
+    
+    l = ['Alpha', 'p-value_alpha']
+    
+    for i in range(1, len(exog_names)):
+        
+        l.append("beta: " + exog_names[i])
+        l.append("p-value_beta: "+ exog_names[i])
+    
+    l.append("R-Squared")
+
+    try:
+        endog_names = list(y.columns)
+        result = pd.DataFrame(index = endog_names, columns = l)
+        
+    except:
+        endog_names = [y.name]
+        result = pd.DataFrame(index = endog_names, columns = l)
+    
+    reg = [] 
+    
+    for i in endog_names:
+        
+        try:  
+            Res1 = sm . OLS ( y[i] ,X). fit ()
+            Res1.summary()
+            
+        except:
+            Res1 = sm . OLS (y ,X). fit ()
+
+        r2 = Res1.rsquared
+        param = Res1.params
+        pval = Res1.pvalues
+        reg.append(Res1)
+        
+        l_val = []
+    
+        for j in range(len(param)):
+            
+            l_val.extend([param[j],pval[j]])
+        
+        l_val.append(r2)
+    
+        result.loc[i] = l_val    
+    
+    return result, reg
+
+def comparison_scatter(df_stocks, df_portfolios, market,
+                       title,xlabel,ylabel,
+                       sigla,string_to_save):
+    cwd = os.getcwd()
+    folder = cwd + "/"+string_to_save
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    fig = plt.figure()
+    ax1 = fig.add_subplot()
+    
+    for i in df_stocks.columns:
+        ax1.scatter(market, df_stocks.loc[:,i], 
+                    c = 'silver', alpha = 0.5)
+    plt.title(title)
+    plt . xlabel (xlabel)
+    plt . ylabel (ylabel)
+
+    
+    ax1.scatter(market, df_portfolios, c = 'black')
+    
+    plt.savefig(folder+"/"+sigla+"-"+i+".png")
+    plt.show()
+
+
+def comparison_scatter_2(df_stocks, df_portfolios, market,
+                       CAPM_Port,
+                       title,xlabel,ylabel,
+                       sigla,string_to_save):
+    cwd = os.getcwd()
+    folder = cwd + "/"+string_to_save
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    fig = plt.figure()
+    
+    ax1 = fig.add_subplot()
+    
+    for i in df_stocks.columns:
+        ax1.scatter(market, df_stocks.loc[:,i], 
+                    c = 'silver', alpha = 0.5)
+    plt.title(title)
+    plt . xlabel (xlabel)
+    plt . ylabel (ylabel)
+
+    
+    ax1.scatter(market, df_portfolios, c = 'black')
+    ax1.plot(market, CAPM_Port.loc['Portfolio - EW','beta: Market']*
+             market+CAPM_Port.loc['Portfolio - EW','Alpha'])
+    
+    plt.savefig(folder+"/"+sigla+"-"+i+".png")
+    plt.show()
+        
+def m_scatter(CAPM_summary, df_factors, df_stocks,
+              sheet,
+              string_to_save):
+    cwd = os.getcwd()
+    folder = cwd + "/2_scatter_comparison/"
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    
+    
+    x = list(CAPM_summary.index)
+    y = x[:3] + x[-1:-4:-1]
+
+
+
+    figure, axis = plt.subplots(2, 3) 
+
+    for i in range(3):
+
+        axis[0,i].scatter(df_factors['Market'],
+                                df_stocks.loc[:,y[i]])
+
+    for i in range(3,6):
+        axis[1,i-3].scatter(df_factors['Market'],
+                                df_stocks.loc[:,y[i]]) 
+    
+    plt.savefig(folder+"/"+string_to_save+".png")
+
+    plt.show()
