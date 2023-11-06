00ecb567916c481e627a1405295bc0965e299d09
RiccardoForni <67865525+RiccardoForni@users.noreply.github.com>
2023-11-06 12:13:32 +0100

    Update Extracode.py

--- Experiment_Code/Extracode.py
+++ Experiment_Code/Extracode.py
@@ -43,3 +43,68 @@ for e in result:
 
 plt.close('all')
 """
+
+"""
+def OLS(y, *x):
+    try: 
+        intercept = pd.Series(data = np.ones_like(x[0]), name = "intercept",
+                              index = x[0].index)
+        
+    except:
+        intercept = pd.DataFrame(data = np.ones(y.shape[0] ), 
+                              columns = ["intercept"],
+                              index = y.index)
+    
+    try:
+        X = pd.DataFrame([intercept, *x]).T
+
+    except:
+        X = pd.concat([intercept,*x],axis = 1)
+
+    exog_names = list(X.columns)
+    
+    l = ['Alpha', 'p-value_alpha']
+    
+    for i in range(1, len(exog_names)):
+        
+        l.append("beta: " + exog_names[i])
+        l.append("p-value_beta: "+ exog_names[i])
+    
+    l.append("R-Squared")
+
+    try:
+        endog_names = list(y.columns)
+        result = pd.DataFrame(index = endog_names, columns = l)
+        
+    except:
+        endog_names = [y.name]
+        result = pd.DataFrame(index = endog_names, columns = l)
+    
+    reg = [] 
+    
+    for i in endog_names:
+        
+        try:  
+            Res1 = sm . OLS ( y[i] ,X). fit ()
+            Res1.summary()
+            
+        except:
+            Res1 = sm . OLS (y ,X). fit ()
+
+        r2 = Res1.rsquared
+        param = Res1.params
+        pval = Res1.pvalues
+        reg.append(Res1)
+        
+        l_val = []
+    
+        for j in range(len(param)):
+            
+            l_val.extend([param[j],pval[j]])
+        
+        l_val.append(r2)
+    
+        result.loc[i] = l_val    
+    
+    return result, reg
+    """
